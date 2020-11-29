e775493c7b5f80d26cd929ffe55258afafe95e11
Patryk <241490@student.pwr.edu.pl>
2020-11-29 11:41:59 +0100

    Small changes

--- common/preprocess.py
+++ common/preprocess.py
@@ -1,12 +1,29 @@
 from common.data_loader import save_numpy_titles, save_numpy_stars, file_loader
+import os
+import shutil
 
 def preprocess_stars(file_in: str):
+    try:
+        shutil.rmtree("source/stars")
+    except:
+        pass
+    os.mkdir("source/stars")
+    os.mkdir("source/stars/data")
+
     gen = file_loader(file_in)
     save_numpy_stars(gen, "review/text", "review/score")
     
 
 
 def preprocess_titles(file_in: str):
+    try:
+        shutil.rmtree("source/titles")
+    except:
+        pass
+    os.mkdir("source/titles")
+    os.mkdir("source/titles/reviews")
+    os.mkdir("source/titles/titles")
+    
     gen = file_loader(file_in)
     save_numpy_titles(gen, "review/text", "review/summary")
 
