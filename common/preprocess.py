1037085b61b47406302b04a0a7af4f68a516455e
Patryk <241490@student.pwr.edu.pl>
2020-11-29 15:27:47 +0100

    Added statistics

--- common/preprocess.py
+++ common/preprocess.py
@@ -2,7 +2,18 @@ from common.data_loader import save_numpy_titles, save_numpy_stars, file_loader
 import os
 import shutil
 
-def preprocess_stars(file_in: str):
+def preprocess_stars(file_in: str) -> None:
+    """This function prepares data to be used by stars prediction
+    
+    Parameters
+    ----------
+    file_in: str
+        Input file name. In our case probably Cell_Phones_&_Accessories.txt
+    Returns
+    ----------
+    None
+    """
+
     try:
         shutil.rmtree("source/stars")
     except:
@@ -15,7 +26,17 @@ def preprocess_stars(file_in: str):
     
 
 
-def preprocess_titles(file_in: str):
+def preprocess_titles(file_in: str) -> None:
+    """This function prepares data to be used by summaries prediction
+    
+    Parameters
+    ----------
+    file_in: str
+        Input file name. In our case probably Cell_Phones_&_Accessories.txt
+    Returns
+    ----------
+    None
+    """
     try:
         shutil.rmtree("source/titles")
     except:
@@ -27,6 +48,38 @@ def preprocess_titles(file_in: str):
     gen = file_loader(file_in)
     save_numpy_titles(gen, "review/text", "review/summary")
 
+
+def preprocess_dict(d: dict):
+    """This function preprocesses dictionary.
+
+    This function preprocesses dictionary. It basically converst data to proper data types. Changes origial dictionary!
+    
+    Parameters
+    ----------
+    d: str
+        Dictionary to be preprocessed
+    Returns
+    ----------
+    None
+    """
+    for entry in d:
+        # Preprocess price
+        if entry["product/price"] == "unknown":
+            entry["product/price"] = None
+        else:
+            entry["product/price"] = float(entry["product/price"])
+        # Preprocess userId
+        if entry["review/userId"] == "unknown":
+            entry["review/userId"] = None
+        # Preprocess helpfulness
+        sl: int = entry["review/helpfulness"].find("/")
+        entry["review/helpfulPositiveVotes"] = int(entry["review/helpfulness"][0:sl])
+        entry["review/helpfulAllVotes"] = int(entry["review/helpfulness"][sl+1:])
+        # Preprocess score
+        entry["review/score"] = int(entry["review/score"].replace(".0", ""))
+        # Preprocess time
+        entry["review/time"] = int(entry["review/time"])
+    
 
 if __name__ == "__main__":
     preprocess_stars("source/Cell_Phones_&_Accessories.txt")
